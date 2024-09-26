6144612e9e3aa49bb5f53e11047b96a618497a70
Brayan Iglesias <iglesiasbrayan2@gmail.com>
2024-09-26 20:08:14 +0200

    Ej 20 ok

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -7,10 +7,20 @@ public class Exercise20 {
 
     public static List<Person> getPeopleList() {
 
+        List<Person> personas = new ArrayList<>();
+        personas.add(new Person("John","Smith"));
+        personas.add(new Teacher("Maria","Montessori","Educacion"));
+        personas.add(new PoliceOfficer("Jake","Peralta","B-99"));
+        personas.add(new Doctor("Gregory","House","Nefrologia y infectologia"));
+
+        return personas;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
 
+        for (Person p : stringList){
+            p.getDetails();
+        }
     }
 
     public static void main(String[] args) {
